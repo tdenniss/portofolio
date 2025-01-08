80069933ab3b504d1cdbbe50916944073d7a2eab
Teglas Dennis <teglasdennis@yahoo.com>
2025-01-08 09:57:56 +0200

    split portofolio into different components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactinfoComponent } from './contactinfo/contactinfo.component';
 
 export const routes: Routes = [
-    { path: "**", component: AppComponent, pathMatch: 'full' }
+    { path: "contact", component: ContactinfoComponent},
+    { path: "projects", component: ProjectsComponent},
+    { path: "about-me", component: AboutmeComponent},
+    { path: "**", component: HomeComponent, pathMatch: 'full' }
 ];
---
+++ src/app/contactinfo/contactinfo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactinfo',
+  standalone: true,
+  imports: [],
+  templateUrl: './contactinfo.component.html',
+  styleUrl: './contactinfo.component.scss'
+})
+export class ContactinfoComponent {
+
+}
