4423045a393f93a751a4ae1c32a2eb6fff748f44
Teglas Dennis <teglasdennis@yahoo.com>
2025-01-08 09:30:15 +0200

    test angular settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: "**", component: AppComponent }
+    { path: "**", component: AppComponent, pathMatch: 'full' }
 ];
