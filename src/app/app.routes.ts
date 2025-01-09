8a3949572dd44aa461bc5e34e813f25190e80499
Teglas Dennis <teglasdennis@yahoo.com>
2025-01-09 09:40:25 +0200

    added routerlink to navbar llinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactinfoComponent } from './contactinfo/contactinfo.component';
 
 export const routes: Routes = [
     { path: "contact", component: ContactinfoComponent},
     { path: "projects", component: ProjectsComponent},
-    { path: "about-me", component: AboutmeComponent},
     { path: "**", component: HomeComponent, pathMatch: 'full' }
 ];
