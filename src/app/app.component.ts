61cf6d46f43ddadddc9193538ea7354fda2ee3fb
Teglas Dennis <teglasdennis@yahoo.com>
2025-01-15 13:31:48 +0200

    feat: tweaks to home and fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { GitHubService } from './github.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [GitHubService]
 })
 export class AppComponent {
   title = 'portofolio';
