6ab1847164f5ab59348e336a2137dac8d8d38266
Teglas Dennis <teglasdennis@yahoo.com>
2025-01-15 13:31:11 +0200

    feat: new contactinfo component

--- src/app/contactinfo/contactinfo.component.ts
+++ src/app/contactinfo/contactinfo.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GitHubService } from '../github.service';
+import { GithubUser } from '../githubuser';
 
 @Component({
   selector: 'app-contactinfo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contactinfo.component.html',
   styleUrl: './contactinfo.component.scss'
 })
-export class ContactinfoComponent {
+export class ContactinfoComponent implements OnInit {
+  githubProfile?: GithubUser;
+  isLoading = true;
+  error?: string;
 
+  constructor(private githubService: GitHubService) {}
+
+  ngOnInit() {
+    this.githubService.getUserProfile().subscribe({
+      next: (profile) => {
+        this.githubProfile = profile;
+        this.isLoading = false;
+      },
+      error: (error: Error) => {
+        this.error = 'Failed to load GitHub profile';
+        this.isLoading = false;
+      }
+    });
+  }
 }
