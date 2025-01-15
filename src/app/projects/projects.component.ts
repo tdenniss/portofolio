6f364c2afd8c757b8f97197abde680dd3772f6fc
Teglas Dennis <teglasdennis@yahoo.com>
2025-01-15 13:32:38 +0200

    feat: projects page now loads info dynamically

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitHubService } from '../github.service';
+import { Repository } from '../repository';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  standalone: true,
-  imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  repositories: Repository[] = [];
+  isLoading = true;
+  error: string | null = null;
 
+  constructor(private githubService: GitHubService) {}
+
+  ngOnInit(): void {
+    this.githubService.getRepositories().subscribe({
+      next: (repos) => {
+        this.repositories = repos;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load projects';
+        this.isLoading = false;
+      }
+    });
+  }
 }
